3edadee20ada0c025f825d9268cc4b9f3cdaf342
lizarragajorge <lizarraga-jorge@live.com>
2018-02-18 14:43:58 -0600

    Adding more unit tests. Modified structure to lambdas.

--- src/game/MasterMind.java
+++ src/game/MasterMind.java
@@ -4,42 +4,87 @@ import java.awt.*;
 import java.util.*;
 import java.util.List;
 
+import java.util.stream.IntStream;
+import java.util.function.IntFunction;
+import java.util.function.Function;
+
+import static java.awt.Color.*;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.counting;
+
+import static game.MasterMind.Feedback.*;
+import static game.MasterMind.GameState.*;
+
 public class MasterMind
 {
-    public enum Feedback {NO_MATCH, MATCH, POSITION_MATCH}
+    public enum Feedback { NO_MATCH, MATCH, POSITION_MATCH }
 
-    private List<Color> solutionList;
+    public enum GameState { IN_PROGRESS, LOST, WON}
+
+    private GameState gameState;
+
+    private int userAttempts;
+
+    private List<Color> gameColors = new ArrayList<>(Arrays.asList(GREEN, BLUE, YELLOW, RED, PINK, ORANGE, MAGENTA, GRAY, DARK_GRAY, LIGHT_GRAY));
+
+    private final int SIZE = 6;
+    private final int MAX_ATTEMPTS = 20;
+
+
+    private List<Color> selection;
 
     protected MasterMind(List<Color> listOfColors)
     {
-        solutionList = listOfColors;
+        selection = listOfColors;
+        gameState = IN_PROGRESS;
+        userAttempts = 0;
     }
 
-    public Map<Feedback, Integer> guess(List<Color> userGuessList)
+    public MasterMind()
     {
-        Map<Feedback, Integer> feedbackList = new HashMap<>();
-
-        for (int solutionListIndex = 0; solutionListIndex < 6; solutionListIndex++)
-        {
-            for (int guessListIndex = 0; guessListIndex < 6; guessListIndex++)
-            {
-                if (solutionList.get(solutionListIndex) == userGuessList.get(guessListIndex) && solutionListIndex == guessListIndex)
-                {
-                    feedbackList.put(Feedback.POSITION_MATCH, feedbackList.getOrDefault(Feedback.POSITION_MATCH, 0) + 1);
-                    break;
-                }
-
-                else if (solutionList.get(solutionListIndex) == userGuessList.get(guessListIndex))
-                {
-                    feedbackList.put(Feedback.MATCH, feedbackList.getOrDefault(Feedback.MATCH, 0) + 1);
-                    break;
-                }
-            }
-        }
-
-        feedbackList.put(Feedback.NO_MATCH, 6 - (feedbackList.getOrDefault(Feedback.POSITION_MATCH, 0) + feedbackList.getOrDefault(Feedback.MATCH, 0)));
-
-        return feedbackList;
+        selection = generateRandomColors();
+        gameState = IN_PROGRESS;
+        userAttempts = 0;
     }
 
+    protected List<Color> generateRandomColors()
+    {
+         Collections.shuffle(gameColors);
+
+         return gameColors.subList(0, SIZE);
+    }
+
+    public Map<Feedback, Long> guess(List<Color> userGuess)
+    {
+        IntFunction<Feedback> computeMatchAtPosition = index ->
+          selection.get(index) == userGuess.get(index) ? POSITION_MATCH :
+            userGuess.contains(selection.get(index)) ? MATCH : NO_MATCH;
+
+         Map<Feedback, Long> feedback =
+           IntStream.range(0, SIZE)
+             .mapToObj(computeMatchAtPosition)
+             .collect((groupingBy(Function.identity(), counting())));
+
+         feedback.computeIfAbsent(NO_MATCH, key -> 0L);
+         feedback.computeIfAbsent(MATCH, key -> 0L);
+         feedback.computeIfAbsent(POSITION_MATCH, key -> 0L);
+
+        return feedback;
+    }
+
+    public void guessEvaluation(Map<Feedback, Long> guess)
+    {
+
+        if (guess.get(POSITION_MATCH) == SIZE && userAttempts <= MAX_ATTEMPTS)
+            gameState = WON;
+        else if (userAttempts < MAX_ATTEMPTS - 1)
+            userAttempts = userAttempts + 1;
+        else
+            gameState = LOST;
+    }
+
+    public GameState getState()
+    {
+        return gameState;
+    }
 }
--- test/game/MasterMindTest.java
+++ test/game/MasterMindTest.java
@@ -8,9 +8,12 @@ import java.awt.Color;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import game.MasterMind.Feedback;
 import static game.MasterMind.Feedback.*;
+import static game.MasterMind.GameState.*;
 
 public class MasterMindTest
 {
@@ -34,9 +37,12 @@ public class MasterMindTest
     {
         List<Color> userGuess = Arrays.asList(Color.PINK, Color.PINK, Color.PINK, Color.PINK, Color.PINK, Color.PINK);
 
-        Map<Feedback, Integer> feedbackList = masterMind.guess(userGuess);
+        Map<Feedback, Long> feedback = masterMind.guess(userGuess);
 
-        assertTrue(feedbackList.get(NO_MATCH) == 6);
+        assertAll(
+                () -> assertEquals(6, (long)feedback.get(NO_MATCH)),
+                () -> assertEquals(0, (long)feedback.get(MATCH)),
+                () -> assertEquals(0, (long)feedback.get(POSITION_MATCH)));
     }
 
     @Test
@@ -44,9 +50,12 @@ public class MasterMindTest
     {
         List<Color> userGuess = Arrays.asList(Color.CYAN, Color.BLACK, Color.WHITE, Color.GREEN, Color.BLUE, Color.RED);
 
-        Map<Feedback, Integer> feedbackList = masterMind.guess(userGuess);
+        Map<Feedback, Long> feedback = masterMind.guess(userGuess);
 
-        assertTrue(feedbackList.get(MATCH) == 6);
+        assertAll(
+                () -> assertEquals(0, (long)feedback.get(NO_MATCH)),
+                () -> assertEquals(6, (long)feedback.get(MATCH)),
+                () -> assertEquals(0, (long)feedback.get(POSITION_MATCH)));
     }
 
     @Test
@@ -54,9 +63,12 @@ public class MasterMindTest
     {
         List<Color> userGuess = Arrays.asList(Color.RED, Color.BLUE, Color.GREEN, Color.WHITE, Color.BLACK, Color.CYAN);
 
-        Map<Feedback, Integer> feedbackList = masterMind.guess(userGuess);
+        Map<Feedback, Long> feedback = masterMind.guess(userGuess);
 
-        assertTrue(feedbackList.get(POSITION_MATCH) == 6);
+        assertAll(
+                () -> assertEquals(0, (long)feedback.get(NO_MATCH)),
+                () -> assertEquals(0, (long)feedback.get(MATCH)),
+                () -> assertEquals(6, (long)feedback.get(POSITION_MATCH)));
     }
 
     @Test
@@ -64,9 +76,12 @@ public class MasterMindTest
     {
         List<Color> userGuess = Arrays.asList(Color.PINK, Color.PINK, Color.PINK, Color.PINK, Color.PINK, Color.RED);
 
-        Map<Feedback, Integer> feedbackList = masterMind.guess(userGuess);
+        Map<Feedback, Long> feedback = masterMind.guess(userGuess);
 
-        assertTrue(feedbackList.get(MATCH) == 1 && feedbackList.get(NO_MATCH) == 5);
+        assertAll(
+                () -> assertEquals(5, (long)feedback.get(NO_MATCH)),
+                () -> assertEquals(1, (long)feedback.get(MATCH)),
+                () -> assertEquals(0, (long)feedback.get(POSITION_MATCH)));
     }
 
     @Test
@@ -74,9 +89,12 @@ public class MasterMindTest
     {
         List<Color> userGuess = Arrays.asList(Color.PINK, Color.BLUE, Color.PINK, Color.PINK, Color.PINK, Color.PINK);
 
-        Map<Feedback, Integer> feedbackList = masterMind.guess(userGuess);
+        Map<Feedback, Long> feedback = masterMind.guess(userGuess);
 
-        assertTrue(feedbackList.get(POSITION_MATCH) == 1 && feedbackList.get(NO_MATCH) == 5);
+        assertAll(
+                () -> assertEquals(5, (long)feedback.get(NO_MATCH)),
+                () -> assertEquals(0, (long)feedback.get(MATCH)),
+                () -> assertEquals(1, (long)feedback.get(POSITION_MATCH)));
     }
 
     @Test
@@ -84,9 +102,12 @@ public class MasterMindTest
     {
         List<Color> userGuess = Arrays.asList(Color.RED, Color.PINK, Color.PINK, Color.PINK, Color.PINK, Color.GREEN);
 
-        Map<Feedback, Integer> feedbackList = masterMind.guess(userGuess);
+        Map<Feedback, Long> feedback = masterMind.guess(userGuess);
 
-        assertTrue(feedbackList.get(POSITION_MATCH) == 1 && feedbackList.get(MATCH) == 1 && feedbackList.get(NO_MATCH) == 4);
+        assertAll(
+                () -> assertEquals(4, (long)feedback.get(NO_MATCH)),
+                () -> assertEquals(1, (long)feedback.get(MATCH)),
+                () -> assertEquals(1, (long)feedback.get(POSITION_MATCH)));
     }
 
     @Test
@@ -94,9 +115,12 @@ public class MasterMindTest
     {
         List<Color> userGuess = Arrays.asList(Color.RED, Color.GREEN, Color.BLUE, Color.WHITE, Color.BLACK, Color.WHITE);
 
-        Map<Feedback, Integer> feedbackList = masterMind.guess(userGuess);
+        Map<Feedback, Long> feedback = masterMind.guess(userGuess);
 
-        assertTrue(feedbackList.get(POSITION_MATCH) == 3 && feedbackList.get(MATCH) == 2 && feedbackList.get(NO_MATCH) == 1);
+        assertAll(
+                () -> assertEquals(1, (long)feedback.get(NO_MATCH)),
+                () -> assertEquals(2, (long)feedback.get(MATCH)),
+                () -> assertEquals(3, (long)feedback.get(POSITION_MATCH)));
     }
 
     @Test
@@ -104,9 +128,12 @@ public class MasterMindTest
     {
         List<Color> userGuess = Arrays.asList(Color.PINK, Color.RED, Color.RED, Color.RED, Color.RED, Color.RED);
 
-        Map<Feedback, Integer> feedbackList = masterMind.guess(userGuess);
+        Map<Feedback, Long> feedback = masterMind.guess(userGuess);
 
-        assertTrue(feedbackList.get(MATCH) == 1 && feedbackList.get(NO_MATCH) == 5);
+        assertAll(
+                () -> assertEquals(5, (long)feedback.get(NO_MATCH)),
+                () -> assertEquals(1, (long)feedback.get(MATCH)),
+                () -> assertEquals(0, (long)feedback.get(POSITION_MATCH)));
     }
 
     @Test
@@ -114,16 +141,98 @@ public class MasterMindTest
     {
         List<Color> userGuess = Arrays.asList(Color.RED, Color.RED, Color.RED, Color.WHITE, Color.WHITE, Color.WHITE);
 
-        Map<Feedback, Integer> feedbackList = masterMind.guess(userGuess);
+        Map<Feedback, Long> feedback = masterMind.guess(userGuess);
 
-        assertTrue(feedbackList.get(POSITION_MATCH) == 2 && feedbackList.get(NO_MATCH) == 4);
+        assertAll(
+                () -> assertEquals(4, (long)feedback.get(NO_MATCH)),
+                () -> assertEquals(0, (long)feedback.get(MATCH)),
+                () -> assertEquals(2, (long)feedback.get(POSITION_MATCH)));
     }
+
+    @Test
+    void gameStatusAtTheStartOfTheGameReturnsInProgress()
+    {
+        assertEquals(IN_PROGRESS, masterMind.getState());
+    }
+
+    @Test
+    void gameStatusAfterTwoGuessesReturnsInProgress()
+    {
+        masterMind.guess(Arrays.asList(Color.PINK, Color.PINK, Color.PINK, Color.PINK, Color.PINK, Color.RED));
+        masterMind.guess(Arrays.asList(Color.PINK, Color.PINK, Color.PINK, Color.PINK, Color.PINK, Color.RED));
+
+        assertEquals(IN_PROGRESS, masterMind.getState());
+    }
+
+    @Test
+    void gameStatusAfterCorrectColorsInAllPositionsReturnsWonState()
+    {
+        Map<Feedback, Long> guess = masterMind.guess(Arrays.asList(Color.RED, Color.BLUE, Color.GREEN, Color.WHITE, Color.BLACK, Color.CYAN));
+
+        masterMind.guessEvaluation(guess);
+
+        assertEquals(WON, masterMind.getState());
+    }
+
+    @Test
+    void gameStatusAfterGuessIsCalledMaxAttemptsReturnsLostState()
+    {
+        Map<Feedback, Long> guess = masterMind.guess(Arrays.asList(Color.RED, Color.RED, Color.GREEN, Color.WHITE, Color.BLACK, Color.CYAN));
+
+        IntStream.range(0,20).forEach(time -> masterMind.guessEvaluation(guess));
+
+        assertEquals(LOST, masterMind.getState());
+    }
+
+    @Test
+    void gameStatusAfterGuessIsCalledAfterCorrectGuessIsWon()
+    {
+        Map<Feedback, Long> guess = masterMind.guess(Arrays.asList(Color.RED, Color.BLUE, Color.GREEN, Color.WHITE, Color.BLACK, Color.CYAN));
+
+        IntStream.range(0,2).forEach(time -> masterMind.guessEvaluation(guess));
+
+        assertEquals(WON, masterMind.getState());
+    }
+
+    @Test
+    void gameStatusAfterGuessIsCalledOnTheLastAttempt()
+    {
+        Map<Feedback, Long> wrongGuess = masterMind.guess(Arrays.asList(Color.RED, Color.RED, Color.GREEN, Color.WHITE, Color.BLACK, Color.CYAN));
+        Map<Feedback, Long> rightGuess = masterMind.guess(Arrays.asList(Color.RED, Color.BLUE, Color.GREEN, Color.WHITE, Color.BLACK, Color.CYAN));
+
+        IntStream.range(0,19).forEach(time -> masterMind.guessEvaluation(wrongGuess));
+        masterMind.guessEvaluation(rightGuess);
+
+        assertEquals(WON, masterMind.getState());
+    }
+
+    @Test
+    void generateRandomColorsReturnsASetOfColors()
+    {
+        assertFalse(masterMind.generateRandomColors().isEmpty());
+    }
+
+    @Test
+    void generateRandomColorsReturnsSixColors()
+    {
+        assertEquals(6, masterMind.generateRandomColors().size());
+    }
+
+    @Test
+    void generateRandomColorsReturnsUniqueColors()
+    {
+        List<Color> distinctlyFilteredColorList = masterMind.generateRandomColors().stream().distinct().collect(Collectors.toList());
+
+        assertEquals(6, distinctlyFilteredColorList.size());
+    }
+
+    
 
     @Test
     void testsToSuppressRedInCoverage()
     {
-      Feedback.values();
-      Feedback.valueOf("NO_MATCH");
-      assertTrue(true);
+        Feedback.values();
+        Feedback.valueOf("NO_MATCH");
+        assertTrue(true);
     }
 }
